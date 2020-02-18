2851757b8b8f69285bec3286e9458b1a6a69ca7e
naveen2493 <naveenkumarsadasivam93@gmail.com>
2020-02-18 10:00:58 +0530

    menu bar and tabs navigation added

--- schoolAppFrontend/src/app/app.component.ts
+++ schoolAppFrontend/src/app/app.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -10,10 +11,13 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
   styleUrls: ['app.component.scss']
 })
 export class AppComponent {
+  pages:any[]=[];
+
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    private router: Router
   ) {
     this.initializeApp();
   }
@@ -22,6 +26,25 @@ export class AppComponent {
     this.platform.ready().then(() => {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
+      this.pages = [{
+        pagename: "Home",
+        icon: "home",
+        url: "tabs/tab1"
+      },
+      {
+        pagename: "Homework",
+        icon: "book",
+        url: "tabs/tab2"
+      },
+      {
+        pagename: "Timetable",
+        icon: "calendar",
+        url: "tabs/tab3"
+      }]
     });
+  }
+
+  goTo(page) {
+    this.router.navigate([page.url]);
   }
 }
