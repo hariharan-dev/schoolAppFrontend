d3f92570c08d18b87262023c8033d7aeb0330172
naveen2493 <naveenkumarsadasivam93@gmail.com>
2020-03-13 23:25:51 +0530

    calendar issue fixed

--- schoolAppFrontend/src/app/pages/homework/homework.module.ts
+++ schoolAppFrontend/src/app/pages/homework/homework.module.ts
@@ -1,10 +1,11 @@
 import { IonicModule } from '@ionic/angular';
 import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
-import { MbscModule } from '@mobiscroll/angular';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { HomeworkPage } from './homework.page';
+
+import { CalendarModule } from '@syncfusion/ej2-angular-calendars';
 
 
 @NgModule({
@@ -12,7 +13,7 @@ import { HomeworkPage } from './homework.page';
     IonicModule,
     CommonModule,
     FormsModule,
-    MbscModule,
+    CalendarModule,
     RouterModule.forChild([{ path: '', component: HomeworkPage }])
     ],
   declarations: [HomeworkPage]
