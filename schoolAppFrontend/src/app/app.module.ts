bc6e3513c9c1beddcd4737d2839803dce1513b5b
naveen2493 <naveenkumarsadasivam93@gmail.com>
2020-03-21 14:29:23 +0530

    exam dates feature

--- schoolAppFrontend/src/app/app.module.ts
+++ schoolAppFrontend/src/app/app.module.ts
@@ -13,13 +13,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { IonicStorageModule } from '@ionic/storage';
 import { LoginPage } from './pages/login/login.page';
-import { ExamDatesPage } from './pages/exam-dates/exam-dates.page';
-import { ExamSyllabusPage } from './pages/exam-syllabus/exam-syllabus.page';
-import { ReportCardPage } from './pages/report-card/report-card.page';
 
 
 @NgModule({
-  declarations: [AppComponent, LoginPage, ExamDatesPage, ExamSyllabusPage, ReportCardPage],
+  declarations: [AppComponent, LoginPage],
   entryComponents: [],
   imports: [
     FormsModule,
